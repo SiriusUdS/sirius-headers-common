4500ed57395e967c71a54884ec9439d983266da7
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2024-12-10 02:31:09 -0500

    Add packet to accelerometer

---
+++ Accelerometer/AccelerometerPacket.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "AccelerometerData.h"
+#include "../TelecommunicationVariables.h"
+
+#define ACCELEROMETER_AMOUNT 1
+
+typedef union {
+  struct {
+    TelecommunicationHeader header;
+    AccelerometerData rawData[ACCELEROMETER_AMOUNT];
+  }
+  packet;
+  uint8_t data[HEADER_SIZE_BYTE + (ACCELEROMETER_AMOUNT * ACCELEROMETER_DATA_SIZE_BYTE)];
+}
+AccelerometerPacket;
