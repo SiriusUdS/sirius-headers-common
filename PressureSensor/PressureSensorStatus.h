d2d3ecc58fbc64c96ed348dc2db81d7071ad3cda
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-01-20 02:01:47 -0500

    Revert negative logic for init(so default state is uninit)

--- PressureSensor/PressureSensorStatus.h
+++ PressureSensor/PressureSensorStatus.h
@@ -4,7 +4,7 @@
 
 typedef union {
   struct {
-    uint16_t notInitialized : 1;
+    uint16_t initialized : 1;
     uint16_t invalidFunctionPointer : 1;
     uint16_t notConnected : 1;
     uint16_t reserved : 13;
