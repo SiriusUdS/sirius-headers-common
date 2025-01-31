b54c367d466b334a41ed12316345ea5cbf1d1701
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-01-31 17:53:30 -0500

    Correct duplicate attribute

---
+++ Valve/ValveErrorStatus.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef union {
+  struct {
+    uint16_t initialized : 1;
+    uint16_t invalidFunctionPointer : 1;
+    uint16_t notConnected : 1;
+    uint16_t RESERVED : 1;
+
+    uint16_t dutyCycleBelowMin : 1;
+    uint16_t dutyCycleOverMax : 1;
+    uint16_t RESERVED2 : 10;
+  }
+  bits;
+  uint16_t value;
+}
+ValveErrorStatus;
--- Valve/ValveStatus.h
+++ Valve/ValveStatus.h
@@ -4,14 +4,7 @@
 
 typedef union {
   struct {
-    uint16_t initialized : 1;
-    uint16_t invalidFunctionPointer : 1;
-    uint16_t notConnected : 1;
-    uint16_t RESERVED : 1;
-
-    uint16_t PWMValueAdjustedMin : 1;
-    uint16_t PWMValueAdjustedMax : 1;
-    uint16_t RESERVED : 10;
+    uint16_t RESERVED : 16;
   }
   bits;
   uint16_t value;
