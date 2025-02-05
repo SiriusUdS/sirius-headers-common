eecaab39de13e65531c70cc7324de0a49e121fc0
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-02-05 02:18:26 -0500

    Add relevant error cases to adc status

--- LowLevelDriver/ADC/ADCErrorStatus.h
+++ LowLevelDriver/ADC/ADCErrorStatus.h
@@ -7,7 +7,13 @@ typedef union {
     uint16_t notInitialized : 1;
     uint16_t nullFunctionPointer : 1;
     uint16_t defaultFunctionCalled : 1;
-    uint16_t RESERVED : 13;
+
+    uint16_t isDisconnected : 1;
+    uint16_t belowMinAnticipatedValue : 1;
+    uint16_t aboveMaxAnticipatedValue : 1;
+    uint16_t isShortCircuited : 1;
+
+    uint16_t RESERVED : 9;
   }
   bits;
   
