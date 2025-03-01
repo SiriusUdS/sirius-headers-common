3f565e9a421cb82d930f6d49e0de0c6ca9f3b1b8
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-03-01 15:57:10 -0500

    Separate ADC from ADC channels

--- LowLevelDriver/ADC/ADCChannelErrorStatus.h
+++ LowLevelDriver/ADC/ADCChannelErrorStatus.h
@@ -7,11 +7,14 @@ typedef union {
     uint16_t notInitialized         : 1;
     uint16_t nullFunctionPointer    : 1;
     uint16_t defaultFunctionCalled  : 1;
+    uint16_t notConnected           : 1;
 
-    uint16_t RESERVED : 13;
+    uint16_t isShortCircuited : 1;
+
+    uint16_t RESERVED : 11;
   }
   bits;
   
   uint16_t value;
 }
-ADCErrorStatus;
+ADCChannelErrorStatus;
