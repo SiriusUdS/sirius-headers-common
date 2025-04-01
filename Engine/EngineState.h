a1c738e69fb9aadc838667cf09aea61db1a72566
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-03-31 20:29:39 -0400

    Adjust header include type for engine sensors

--- Engine/EngineState.h
+++ Engine/EngineState.h
@@ -1,13 +1,12 @@
 #pragma once
 
-typedef enum {
-  ENGINE_STATE_INIT,
-  ENGINE_STATE_IDLE,
-  ENGINE_STATE_ARMING,
-  ENGINE_STATE_READY,
-  ENGINE_STATE_IGNITION,
-  ENGINE_STATE_POWERED_FLIGHT,
-  ENGINE_STATE_UNPOWERED_FLIGHT,
-  ENGINE_STATE_ABORT
-}
-EngineState;
+#include <stdint.h>
+
+#define ENGINE_STATE_INIT             (uint8_t)0x00
+#define ENGINE_STATE_IDLE             (uint8_t)0x01
+#define ENGINE_STATE_ARMING           (uint8_t)0x02
+#define ENGINE_STATE_ARMED            (uint8_t)0x03
+#define ENGINE_STATE_IGNITION         (uint8_t)0x04
+#define ENGINE_STATE_POWERED_FLIGHT   (uint8_t)0x05
+#define ENGINE_STATE_UNPOWERED_FLIGHT (uint8_t)0x06
+#define ENGINE_STATE_ABORT            (uint8_t)0x07
