049043a3e90f658c026e4e5a3b6133cb340cc199
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-04-19 01:30:05 -0400

    add buttons constants for gs-control

--- GSControl/GSControlSensors.h
+++ GSControl/GSControlSensors.h
@@ -1,17 +1,28 @@
 #pragma once
 
+#include <stdint.h>
+
 /***************************************************/
 /***            Sensors/Devices Amount           ***/
 /***************************************************/
+#define GS_CONTROL_BUTTONS_AMOUT (uint8_t)0x04
 
 /***************************************************/
 /***           Peripheral Distribution           ***/
 /***************************************************/
+#define GS_CONTROL_GPIO_PER_BUTTONS (uint8_t)0x01
 
 /***************************************************/
 /***              Peripheral Amount              ***/
 /***************************************************/
+#define GS_CONTROL_GPIO_AMOUNT GS_CONTROL_BUTTONS_AMOUT * GS_CONTROL_GPIO_PER_BUTTONS
 
 /***************************************************/
-/***                Array Indexes                ***/
+/***       Array Indexes -- Sensors/Devices      ***/
 /***************************************************/
+#define GS_CONTROL_BUTTON_IGNITE (uint8_t)0x00
+
+/***************************************************/
+/***         Array Indexes -- Peripherals        ***/
+/***************************************************/
+#define GS_CONTROL_GPIO_BUTTON_IGNITE (uint8_t)0x00
