9ddcd4d54b4bd823d5bd3c7942ec637dbcce83a0
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-04-18 22:40:36 -0400

    add missing sensors constants for filling station

--- FillingStation/FillingStationSensors.h
+++ FillingStation/FillingStationSensors.h
@@ -3,25 +3,26 @@
 /***************************************************/
 /***            Sensors/Devices Amount           ***/
 /***************************************************/
-#define FILLING_STATION_THERMISTANCE_AMOUNT (uint8_t)0x04
-#define FILLING_STATION_LOAD_CELL_AMOUNT    (uint8_t)0x02
-#define FILLING_STATION_HEATPAD_AMOUNT      (uint8_t)0x04
-#define FILLING_STATION_VALVE_AMOUNT        (uint8_t)0x02
+#define FILLING_STATION_THEMPERATURE_SENSOR_AMOUNT (uint8_t)0x04
+#define FILLING_STATION_LOAD_CELL_AMOUNT           (uint8_t)0x02
+#define FILLING_STATION_PRESSURE_SENSOR_AMOUNT     (uint8_t)0x02
+#define FILLING_STATION_HEATPAD_AMOUNT             (uint8_t)0x04
+#define FILLING_STATION_VALVE_AMOUNT               (uint8_t)0x02
 
 /***************************************************/
 /***           Peripheral Distribution           ***/
 /***************************************************/
-#define FILLING_STATION_PWM_PER_VALVE (uint8_t)0x01
-
-#define FILLING_STATION_GPIO_PER_VALVE   (uint8_t)0x02
-
+#define FILLING_STATION_PWM_PER_VALVE                      (uint8_t)0x01
+#define FILLING_STATION_GPIO_PER_VALVE                     (uint8_t)0x02
 #define FILLING_STATION_ADC_CHANNEL_PER_TEMPERATURE_SENSOR (uint8_t)0x01
 #define FILLING_STATION_ADC_CHANNEL_PER_PRESSURE_SENSOR    (uint8_t)0x01
+
 /***************************************************/
 /***              Peripheral Amount              ***/
 /***************************************************/
-#define FILLING_STATION_ADC_CHANNEL_AMOUNT FILLING_STATION_THERMISTANCE_AMOUNT + FILLING_STATION_LOAD_CELL_AMOUNT
+#define FILLING_STATION_ADC_CHANNEL_AMOUNT FILLING_STATION_THEMPERATURE_SENSOR_AMOUNT + FILLING_STATION_LOAD_CELL_AMOUNT
 #define FILLING_STATION_PWM_AMOUNT         FILLING_STATION_VALVE_AMOUNT
+#define FILLING_STATION_GPIO_AMOUNT        (FILLING_STATION_VALVE_AMOUNT * FILLING_STATION_GPIO_PER_VALVE)
 
 /***************************************************/
 /***                Array Indexes                ***/
