127abe035598c143de67a0999a5062b6a70587f1
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-05-16 19:39:14 -0400

    add valve statuses to status packet

--- Telecommunication/TelemetryPacket.h
+++ Telecommunication/TelemetryPacket.h
@@ -25,6 +25,7 @@ typedef struct {
   uint32_t timestamp_ms;
   uint16_t engineStatus;
   uint16_t engineErrorStatus;
+  uint16_t valvesStatus[ENGINE_VALVE_AMOUNT];
   uint16_t temperatureSensorErrorStatus[ENGINE_TEMPERATURE_SENSOR_AMOUNT];
   uint16_t pressureSensorErrorStatus[ENGINE_PRESSURE_SENSOR_AMOUNT];
   uint32_t crc;
