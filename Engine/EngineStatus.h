d41cbd9b1351151aa34ed1718ffe7113b2cd6078
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-05-16 19:07:27 -0400

    add engine specific telemetry packets

--- Engine/EngineStatus.h
+++ Engine/EngineStatus.h
@@ -6,7 +6,10 @@ typedef union {
   struct {
     uint16_t testModeOn : 1;
     uint16_t fastModeOn : 1;
-    uint16_t RESERVED : 13;
+    uint16_t RESERVED : 2;
+
+    uint16_t state : 6;
+    uint16_t RESERVED2 : 6;
   }
   bits;
   uint16_t value;
---
+++ Telecommunication/TelemetryPacket.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <stdint.h>
+
+#include "TelemetryHeader.h"
+#include "../Engine/EngineSensors.h"
+
+typedef struct {
+  TelemetryHeader header;
+  uint32_t timestamp_ms;
+  uint16_t adcValues[ENGINE_ADC_CHANNEL_AMOUNT];
+  uint32_t crc;
+}
+EngineTelemetryPacketFields;
+
+typedef union {
+  EngineTelemetryPacketFields fields;
+
+  uint8_t data[sizeof(EngineTelemetryPacketFields)];
+}
+EngineTelemetryPacket;
+
+typedef struct {
+  TelemetryHeader header;
+  uint32_t timestamp_ms;
+  uint16_t engineStatus;
+  uint16_t engineErrorStatus;
+  uint16_t temperatureSensorErrorStatus[ENGINE_TEMPERATURE_SENSOR_AMOUNT];
+  uint16_t pressureSensorErrorStatus[ENGINE_PRESSURE_SENSOR_AMOUNT];
+  uint32_t crc;
+}
+EngineStatusPacketFields;
+
+typedef union {
+  EngineStatusPacketFields fields;
+
+  uint8_t data[sizeof(EngineStatusPacketFields)];
+}
+EngineStatusPacket;
