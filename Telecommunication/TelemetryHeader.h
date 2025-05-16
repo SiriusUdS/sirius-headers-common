5165abc4b576defd45ee89f789214227e951cad2
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-05-16 19:20:58 -0400

    add header codes for telemetry packets

--- Telecommunication/TelemetryHeader.h
+++ Telecommunication/TelemetryHeader.h
@@ -2,28 +2,27 @@
 
 #include <stdint.h>
 
-#define TELEMETRY_ACCELEROMETER_DATA_HEADER_CODE      (uint8_t)0x414343 // "ACC"
-#define TELEMETRY_GYROSCOPE_DATA_HEADER_CODE          (uint8_t)0x475952 // "GYR"
-#define TELEMETRY_ALTIMETER_DATA_HEADER_CODE          (uint8_t)0x414C54 // "ALT"
-#define TELEMETRY_GPS_DATA_HEADER_CODE                (uint8_t)0x475053 // "GPS"
-#define TELEMETRY_MAGNETOMETER_DATA_HEADER_CODE       (uint8_t)0x4D4147 // "MAG"
-#define TELEMETRY_PRESSURE_SENSOR_DATA_HEADER_CODE    (uint8_t)0x505253 // "PRS"
-#define TELEMETRY_ROCKET_DATA_HEADER_CODE             (uint8_t)0x524B54 // "RKT"
-#define TELEMETRY_TEMPERATURE_SENSOR_DATA_HEADER_CODE (uint8_t)0x54484D // "THM"
-#define TELEMETRY_VALVE_DATA_HEADER_CODE              (uint8_t)0x564C56 // "VLV"
+#define TELEMETRY_ACCELEROMETER_DATA_HEADER_CODE      (uint32_t)0x414343 // "ACC"
+#define TELEMETRY_GYROSCOPE_DATA_HEADER_CODE          (uint32_t)0x475952 // "GYR"
+#define TELEMETRY_ALTIMETER_DATA_HEADER_CODE          (uint32_t)0x414C54 // "ALT"
+#define TELEMETRY_GPS_DATA_HEADER_CODE                (uint32_t)0x475053 // "GPS"
+#define TELEMETRY_MAGNETOMETER_DATA_HEADER_CODE       (uint32_t)0x4D4147 // "MAG"
+#define TELEMETRY_PRESSURE_SENSOR_DATA_HEADER_CODE    (uint32_t)0x505253 // "PRS"
+#define TELEMETRY_ROCKET_DATA_HEADER_CODE             (uint32_t)0x524B54 // "RKT"
+#define TELEMETRY_TEMPERATURE_SENSOR_DATA_HEADER_CODE (uint32_t)0x54484D // "THM"
+#define TELEMETRY_VALVE_DATA_HEADER_CODE              (uint32_t)0x564C56 // "VLV"
 
-#define TELEMETRY_ENGINE_SOURCE_ID          (uint8_t)0x00
-#define TELEMETRY_FILLING_STATION_SOURCE_ID (uint8_t)0x01
-#define TELEMETRY_GS_CONTROL_SOURCE_ID      (uint8_t)0x02
+#define TELEMETRY_ENGINE_BOARD_ID          (uint8_t)0x00
+#define TELEMETRY_FILLING_STATION_BOARD_ID (uint8_t)0x01
+#define TELEMETRY_GS_CONTROL_BOARD_ID      (uint8_t)0x02
 
-#define TELEMETRY_TEMPERATURE_SENSOR_SENSOR_ID (uint8_t)0x00
+#define TELEMETRY_HEADER_TYPE_TELEMETRY (uint32_t)0x54454C59 // TELY
+#define TELEMETRY_HEADER_TYPE_STATUS    (uint32_t)0x53544155 // STAU
 
 typedef union {
   struct {
-    uint32_t type        : 20;
-    uint32_t sensorType  : 4;
-    uint32_t boardId     : 3;
-    uint32_t sensorIndex : 5;
+    uint32_t type        : 28;
+    uint32_t boardId     : 4;
   }
   bits;
 
