2ec17922e8cdace321f5b60daeccbfda586a18d3
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-05-31 15:32:22 -0400

    Update GSControlSensors.h

--- GSControl/GSControlSensors.h
+++ GSControl/GSControlSensors.h
@@ -23,10 +23,11 @@
 #define GS_CONTROL_BUTTON_ALLOW_FILL_INDEX     (uint8_t)0x00
 #define GS_CONTROL_BUTTON_ARM_VALVE_INDEX      (uint8_t)0x01
 #define GS_CONTROL_BUTTON_ARM_IGNITER_INDEX    (uint8_t)0x02
-#define GS_CONTROL_BUTTON_UNUSED_INDEX         (uint8_t)0x03
+#define GS_CONTROL_BUTTON_ALLOW_DUMP_INDEX     (uint8_t)0x03
 #define GS_CONTROL_BUTTON_EMERGENCY_STOP_INDEX (uint8_t)0x04
 #define GS_CONTROL_BUTTON_FIRE_IGNITER_INDEX   (uint8_t)0x05
 #define GS_CONTROL_BUTTON_UNSAFE_INDEX         (uint8_t)0x06
+#define GS_CONTROL_BUTTON_VALVE_START_INDEX    (uint8_t)0x07
 
 
 /***************************************************/
