b5a062e3ea32644a8363b3aba8e0cdda28e9a8d4
Marc <marcantoine.lavigne.gauthier@gmail.com>
2025-07-07 20:59:05 -0400

    engine_state

--- Engine/EngineState.h
+++ Engine/EngineState.h
@@ -2,12 +2,25 @@
 
 #include <stdint.h>
 
+/**
+ * 
+ * INIT
+ *  |
+ * SAFE  <--.
+ *  |       |
+ * UNSAFE --.
+ *  |
+ *IGNITION
+ *  |
+ *LAUNCH
+ *  |
+ * ..  
+ *
+ */
+
 #define ENGINE_STATE_INIT             (uint8_t)0x00
-#define ENGINE_STATE_IDLE             (uint8_t)0x01
-#define ENGINE_STATE_ABORT            (uint8_t)0x02
-#define ENGINE_STATE_TESTING          (uint8_t)0x03
-#define ENGINE_STATE_ARMING           (uint8_t)0x04
-#define ENGINE_STATE_ARMED            (uint8_t)0x05
+#define ENGINE_STATE_SAFE             (uint8_t)0x01
+#define ENGINE_STATE_UNSAFE            (uint8_t)0x02
 #define ENGINE_STATE_IGNITION         (uint8_t)0x06
 #define ENGINE_STATE_LAUNCH           (uint8_t)0x07
 #define ENGINE_STATE_POWERED_FLIGHT   (uint8_t)0x08
