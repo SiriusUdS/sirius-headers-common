cf13a67bd648ae0f5da9112d52665b35b6502755
WSimard47536 <83228735+WSimard47536@users.noreply.github.com>
2025-07-09 00:03:49 -0400

    adjust states of boards

--- Engine/EngineState.h
+++ Engine/EngineState.h
@@ -20,9 +20,7 @@
 
 #define ENGINE_STATE_INIT             (uint8_t)0x00
 #define ENGINE_STATE_SAFE             (uint8_t)0x01
-#define ENGINE_STATE_UNSAFE            (uint8_t)0x02
-#define ENGINE_STATE_ABORT             (uint8_t)0x03
+#define ENGINE_STATE_UNSAFE           (uint8_t)0x02
+#define ENGINE_STATE_ABORT            (uint8_t)0x03
 #define ENGINE_STATE_IGNITION         (uint8_t)0x06
 #define ENGINE_STATE_LAUNCH           (uint8_t)0x07
-#define ENGINE_STATE_POWERED_FLIGHT   (uint8_t)0x08
-#define ENGINE_STATE_UNPOWERED_FLIGHT (uint8_t)0x09
--- GSControl/GSControlState.h
+++ GSControl/GSControlState.h
@@ -3,5 +3,6 @@
 #include <stdint.h>
 
 #define GS_CONTROL_STATE_INIT    (uint8_t)0x00
-#define GS_CONTROL_STATE_IDLE    (uint8_t)0x01
-#define GS_CONTROL_STATE_ABORT   (uint8_t)0x02
+#define GS_CONTROL_STATE_SAFE    (uint8_t)0x01
+#define GS_CONTROL_STATE_UNSAFE  (uint8_t)0x02
+#define GS_CONTROL_STATE_ABORT   (uint8_t)0x03
