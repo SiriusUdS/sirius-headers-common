d0f709a0136cfe2dfbd40e2ef0ae28b8f29a5d2e
Marc <marcantoine.lavigne.gauthier@gmail.com>
2026-03-06 17:54:55 -0500

    add LC 2026 states

--- Engine/EngineState.h
+++ Engine/EngineState.h
@@ -3,19 +3,7 @@
 #include <stdint.h>
 
 /**
- * 
- * INIT
- *  |
- * SAFE  <--.
- *  |       |
- * UNSAFE --.
- *  |
- *IGNITION
- *  |
- *LAUNCH
- *  |
- * ..  
- *
+ * https://usherbrooke.sharepoint.com/:p:/r/sites/SIRIUS/_layouts/15/Doc2.aspx?action=edit&sourcedoc=%7B2f1058e7-45a9-4e77-bcaa-6724409993bb%7D&wdOrigin=TEAMS-MAGLEV.teamsSdk_ns.rwc&wdExp=TEAMS-TREATMENT&wdhostclicktime=1769475998854&web=1
  */
 
 #define ENGINE_STATE_INIT             (uint8_t)0x00
@@ -23,4 +11,6 @@
 #define ENGINE_STATE_UNSAFE           (uint8_t)0x02
 #define ENGINE_STATE_ABORT            (uint8_t)0x03
 #define ENGINE_STATE_IGNITION         (uint8_t)0x06
-#define ENGINE_STATE_LAUNCH           (uint8_t)0x07
+#define ENGINE_STATE_FIRE             (uint8_t)0x07
+#define ENGINE_STATE_ERROR            (uint8_t)0x08
+#define ENGINE_STATE_TEST             (uint8_t)0x09
--- FillingStation/FillingStationState.h
+++ FillingStation/FillingStationState.h
@@ -2,7 +2,14 @@
 
 #include <stdint.h>
 
+/**
+ * https://usherbrooke.sharepoint.com/:p:/r/sites/SIRIUS/_layouts/15/Doc2.aspx?action=edit&sourcedoc=%7B2f1058e7-45a9-4e77-bcaa-6724409993bb%7D&wdOrigin=TEAMS-MAGLEV.teamsSdk_ns.rwc&wdExp=TEAMS-TREATMENT&wdhostclicktime=1769475998854&web=1
+ */
+
 #define FILLING_STATION_STATE_INIT    (uint8_t)0x00
 #define FILLING_STATION_STATE_SAFE    (uint8_t)0x01
 #define FILLING_STATION_STATE_UNSAFE  (uint8_t)0x02
 #define FILLING_STATION_STATE_ABORT   (uint8_t)0x03
+#define FILLING_STATION_STATE_ERROR   (uint8_t)0x04
+#define FILLING_STATION_STATE_IGNITE  (uint8_t)0x05
+#define FILLING_STATION_STATE_TEST    (uint8_t)0x06
