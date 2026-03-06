a4173c0340f621aab09aa81bd620c3a25b2c8d72
Marc <marcantoine.lavigne.gauthier@gmail.com>
2026-03-06 18:26:16 -0500

    add default udp packets

---
+++ Ethernet/DiagnosePacket.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <stdint.h>
+
+#define DIAGNOSE_PACKET_CODE   (uint8_t)0x02
---
+++ Ethernet/SyncPacket.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <stdint.h>
+
+#define SYNC_PACKET_CODDE   (uint8_t)0x01
+
+typedef struct {
+    uint8_t forceState:1;
+    uint8_t confirmSync:1;
+    uint8_t syncTime:1;
+    uint8_t diagnose:1;
+    uint8_t reserved:4;
+} FrameSyncFlags;
+
+
+typedef union {
+    FrameSyncFlags frame;
+    uint8_t code;
+} SyncFlags;
+
+typedef struct {
+    uint8_t deviceIDFrom:8;
+    uint8_t deviceIDTo:8;
+    uint8_t syncState:8;
+    SyncFlags syncFlags;
+} FrameSyncPacket;
+
+/// @brief This packet sync the desired device with the state from another. Every devices on the network MUST treat this packet.
+typedef union {
+    FrameSyncPacket frame;
+    uint8_t data[sizeof(FrameSyncPacket)];
+} SyncPacket;
