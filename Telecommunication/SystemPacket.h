142cc2cb0c68980393d6685236aab00a4fa4c0e0
Marc <marcantoine.lavigne.gauthier@gmail.com>
2026-06-06 16:50:00 -0400

    sys

--- Telecommunication/SystemPacket.h
+++ Telecommunication/SystemPacket.h
@@ -1,12 +1,14 @@
 #include <stdint.h>
 #include "InterfaceField.h"
 #include "../ADC/Channels.h"
+#include "../Valve/ServoPacket.h"
 
 typedef struct {
     uint32_t frameTs_MS;
     uint32_t lastHandshakeTs_MS;
     InterfaceField interfaces;
     Channel channels[12];
+    ServoPacket valves[2];
 } FrameSystemPacket;
 
 typedef union {
